abfe04fc1857f21d49e858785b797b45627f7af7
Ben Hunter <benphunter@hotmail.com>
2016-03-29 11:16:10 -0700

    Updated retrieve to test.

--- history/history_retrieve_product_views/handler.js
+++ history/history_retrieve_product_views/handler.js
@@ -2,6 +2,6 @@
 
 module.exports.handler = function(event, context) {
   return context.done(null, {
-    message: 'Way to Go Serverless! Your Lambda function executed successfully!'
+    message: 'Nice to Go Serverless! Your Lambda function executed successfully!'
   });
 };
